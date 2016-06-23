113d183ec586d56f886a64c98a7d2b19ea567210
Deament <591518884@qq.com>
2016-06-24 07:31:09 +0800

    用户接口

--- src/cn/going/ui/model/UserI.java
+++ src/cn/going/ui/model/UserI.java
@@ -6,9 +6,7 @@ package cn.going.ui.model;
 import java.io.Serializable;
 
 /**
- * @author yy
- * 2016-06-21
- * 用戶基础类
+ * @author yy 2016-06-21 用戶基础类
  */
 public class UserI implements Serializable {
 	/**
@@ -17,40 +15,34 @@ public class UserI implements Serializable {
 	private static final long serialVersionUID = 8884558988237838150L;
 
 	private Integer id;
-	
-	private String username;//原始登录名
-	private String email;//Email
-	private String phone;//手机号码
-	private String password;//密码
-	private String state;//是否封锁
-	
-	
-	
-	
-	
 
-	
-	
-	
-	
-	
+	private String username;// 原始登录名
+	private String email;// Email
+	private String phone;// 手机号码
+	private String password;// 密码
+	private String state;// 是否封锁
+
+	private String createtime;
+	private String isdelete;
+
 	@Override
 	public String toString() {
-		return "UserI [getState()=" + getState() + ", getId()=" + getId()
-				+ ", getUsername()=" + getUsername() + ", getPassword()="
-				+ getPassword() + ", getEmail()=" + getEmail()
-				+ ", getPhone()=" + getPhone() + "]";
+		return "UserI [id=" + id + ", username=" + username + ", email="
+				+ email + ", phone=" + phone + ", password=" + password
+				+ ", state=" + state + ", createtime=" + createtime
+				+ ", isdelete=" + isdelete + "]";
 	}
-	
-	
-	
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((createtime == null) ? 0 : createtime.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((isdelete == null) ? 0 : isdelete.hashCode());
 		result = prime * result
 				+ ((password == null) ? 0 : password.hashCode());
 		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
@@ -59,9 +51,6 @@ public class UserI implements Serializable {
 				+ ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
-
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -72,6 +61,11 @@ public class UserI implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		UserI other = (UserI) obj;
+		if (createtime == null) {
+			if (other.createtime != null)
+				return false;
+		} else if (!createtime.equals(other.createtime))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
@@ -81,6 +75,11 @@ public class UserI implements Serializable {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (isdelete == null) {
+			if (other.isdelete != null)
+				return false;
+		} else if (!isdelete.equals(other.isdelete))
 			return false;
 		if (password == null) {
 			if (other.password != null)
@@ -105,45 +104,68 @@ public class UserI implements Serializable {
 		return true;
 	}
 
+	public String getCreatetime() {
+		return createtime;
+	}
 
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
 
+	public String getIsdelete() {
+		return isdelete;
+	}
+
+	public void setIsdelete(String isdelete) {
+		this.isdelete = isdelete;
+	}
 
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	
 }
